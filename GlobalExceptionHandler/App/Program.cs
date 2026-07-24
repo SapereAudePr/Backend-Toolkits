17ca87d15b24ce7c1bcf048bce9004340a3f2082
SapereAudePr <capitolsmife@gmail.com>
2026-07-24 14:14:35 +0300

    First commit for GEH: c1 | Learning exception from scratch

---
+++ GlobalExceptionHandler/App/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var result = Divider(int.MinValue, -1);
+        Console.WriteLine(result);
+        var result1 = Divider(2, 0);
+        Console.WriteLine(result1);
+    }
+
+    private static int Divider(int first, int second)
+    {
+        try
+        {
+            return first / second;
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+}
