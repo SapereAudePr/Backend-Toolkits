4bb7c704fd6772c740f9d141f085fbc28a52f938
SapereAudePr <capitolsmife@gmail.com>
2026-08-10 00:34:52 +0300

    Result Pattern practicing started without Result class yet.

---
+++ ResultPattern/ResultPatternApp/BankTransfer.cs
@@ -0,0 +1,26 @@
+﻿namespace ResultPatternApp;
+
+public class BankTransfer(decimal balance)
+{
+    private decimal _balance = balance;
+
+    public bool TryWithdraw(decimal amount, out string reason)
+    {
+        if (amount <= 0)
+        {
+            reason = "Amount can not be equal or lower to 0";
+            return false;
+        }
+
+        if (amount > _balance)
+        {
+            reason = $"Withdraw failed: requested amount {amount:C}, but the available balance is {_balance:C}.";
+            return false;
+        }
+
+        _balance -= amount;
+        Console.WriteLine($"Withdraw amount {amount} and the remaining balance is {_balance}");
+        reason = string.Empty;
+        return true;
+    }
+}
---
+++ ResultPattern/ResultPatternApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ResultPatternApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var bank = new BankTransfer(200);
+        if (!bank.TryWithdraw(190, out var reason))
+        {
+            Console.WriteLine(reason);
+        }
+    }
+}
